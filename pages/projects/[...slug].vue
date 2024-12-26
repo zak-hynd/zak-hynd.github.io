e2534b91ebf08b6eb966a503405aca1b2d21baba
Zak Hynd <zakhynd@gmail.com>
2024-12-27 10:52:47 +1300

    resetup to deploy

--- pages/projects/[...slug].vue
+++ pages/projects/[...slug].vue
@@ -9,10 +9,11 @@
 
 <template>
     <ContentRenderer :value="data" class="prose my-10 mx-auto max-w-7xl" />
-    <div class="my-8">
+    <!-- tags don't seem to like being deployed -->
+    <!-- <div class="my-8">
         <a v-for="tag in  data.tags " :key="tag" :href="`/projects/tags/${tag}`"
             class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4">
             <Icon name="pajamas:label" size="1.5rem" class="text-gray-100 mr-2" /> {{ tag }}
         </a>
-    </div>
+    </div> -->
 </template>
